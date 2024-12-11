139cf8eff9f01b09744d2c6ab811df2704b94a33
Samiimasmoudii <samsoum.tecktonick@gmail.com>
2024-12-11 11:00:56 +0100

    Initial Commit + Compose

---
+++ Spark-Processor/src/main/java/org/example/entity/AverageData.java
@@ -0,0 +1,33 @@
+package org.example.entity;
+import java.io.Serializable;
+
+public class AverageData implements Serializable {
+
+    private String id;
+    private double temperature;
+
+    private double humidity;
+
+    public AverageData() {
+
+    }
+
+    public AverageData(String id, double temperature, double humidity) {
+        super();
+        this.id = id;
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+}
